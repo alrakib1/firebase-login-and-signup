0fd8de53fedd61ef82f8e9ae93a6945f716962c1
alrakib1 <abdullahalrakib30@gmail.com>
2023-10-05 16:30:23 +0600

    added email verification

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,23 +1,54 @@
-import { signInWithEmailAndPassword } from "firebase/auth";
-import React, { useState } from "react";
+import { sendPasswordResetEmail, signInWithEmailAndPassword } from "firebase/auth";
+
+import { useRef, useState } from "react";
 import auth from "../../firebase.config";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   const [signUpError, setSignUpError] = useState("");
   const [success, setSuccess] = useState("");
+  const emailRef = useRef(null);
   const handleLogin = (e) => {
     e.preventDefault();
     const email = e.target.email.value;
     const password = e.target.password.value;
+
+    setSignUpError("");
+    setSuccess("");
+
     signInWithEmailAndPassword(auth, email, password)
       .then((result) => {
         const user = result.user;
-        console.log(user);
+        if(result.user.emailVerified){
+          setSuccess("Login Successfully");
+        }else{
+          alert('please verify your email')
+        }
       })
       .catch((error) => {
-        console.error(error);
+        if (error.message == "Firebase: Error (auth/email-already-in-use).") {
+          setSignUpError("Email Already exists");
+          return;
+        }
       });
   };
+
+  const handleForgetPassword =() =>{
+    const email = emailRef.current.value;
+    if(!email){
+      console.log('please provide an email', emailRef.current.value);
+      return;
+    }else if(!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email)){
+console.log('please write a valid email');
+return;
+    }
+    // send validation email
+    sendPasswordResetEmail(auth,email).then(result=>{
+      alert('please check your email')
+    }).catch(error=>{
+      console.log(error)
+    })
+  }
   return (
     <div className="hero min-h-screen bg-base-200">
       <div className="hero-content flex-col lg:flex-row-reverse">
@@ -37,6 +68,7 @@ const Login = () => {
                   <span className="label-text">Email</span>
                 </label>
                 <input
+                ref={emailRef}
                   type="email"
                   name="email"
                   placeholder="email"
@@ -56,7 +88,7 @@ const Login = () => {
                   required
                 />
                 <label className="label">
-                  <a href="#" className="label-text-alt link link-hover">
+                  <a onClick={handleForgetPassword} href="#" className="label-text-alt link link-hover">
                     Forgot password?
                   </a>
                 </label>
@@ -65,6 +97,9 @@ const Login = () => {
                 <button className="btn btn-primary">Login</button>
               </div>
             </form>
+            {signUpError && <p className="text-red-600">{signUpError}</p>}
+            {success && <p className="text-green-700">{success}</p>}
+            <p>New to our Website ? Please <Link to="/signup"><span className="text-blue-500">Sign-Up</span></Link> </p>
           </div>
         </div>
       </div>
--- src/components/signup/SignUp.jsx
+++ src/components/signup/SignUp.jsx
@@ -1,4 +1,7 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  sendEmailVerification,
+} from "firebase/auth";
 import React, { useState } from "react";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
 import auth from "../../firebase.config";
@@ -9,15 +12,17 @@ const SignUp = () => {
 
   const handleSignUp = (e) => {
     e.preventDefault();
+
+    const name = e.target.name.value;
     const email = e.target.email.value;
     const password = e.target.password.value;
     const accepted = e.target.terms.checked;
 
     setSignUpError("");
     setSuccess("");
+
     const check = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/;
 
-    
     if (password.length < 6) {
       setSignUpError("password must be 6 characters or longer");
       return;
@@ -35,39 +40,52 @@ const SignUp = () => {
       .then((result) => {
         const user = result.user;
         setSuccess("User Created Successfully");
+        // send verification email
+        sendEmailVerification(user).then(() => {
+          alert("please check your email and verify your account");
+        });
       })
       .catch((error) => {
-        if(error.message == 'Firebase: Error (auth/email-already-in-use).'){
-          setSignUpError('Email Already exists')
-          return ;
+        if (error.message == "Firebase: Error (auth/email-already-in-use).") {
+          setSignUpError("User Already exists");
+          return;
         }
       });
   };
   return (
     <>
-      <div className="mx-auto md:w-1/4">
-        <h3 className="text-4xl mb-4 mt-3">Please sign up</h3>
+      <div className="mx-auto lg:w-1/4 w-3/4 relative container flex flex-col justify-center items-center mt-10 text-center ">
+        <h3 className="text-4xl mb-4 mt-3 text-center">Please sign up</h3>
         <form onSubmit={handleSignUp}>
           <input
-            type="email"
-            name="email"
-            placeholder="email address"
+            type="text"
+            name="name"
+            placeholder="Your Name"
             id=""
-            className="mb-4 w-3/4 px-4 py-2"
+            className="mb-4 w-full px-4 py-2 mx-auto"
             required
           />
           <br />
-          <div className="relative">
+          <input
+            type="email"
+            name="email"
+            placeholder="Email address"
+            id=""
+            className="mb-4 w-full px-4 py-2 mx-auto"
+            required
+          />
+          <br />
+          <div className="">
             <input
               type={showPassword ? "text" : "password"}
               name="password"
               id=""
-              className="mb-4 w-3/4 px-4 py-2 pr-10"
-              placeholder="password"
+              className="mb-4 w-full px-4 py-2 pr-10"
+              placeholder="Password"
               required
             />
             <span
-              className="absolute top-3 left-80 cursor-pointer"
+              className="absolute left-[210px] top-[135px] md:left-[380px] lg:left-[340px] cursor-pointer"
               onClick={() => SetShowPassword(!showPassword)}
             >
               {showPassword ? <FaEyeSlash /> : <FaEye />}
@@ -76,14 +94,14 @@ const SignUp = () => {
           <br />
           <div className="mb-2">
             <input type="checkbox" name="terms" id="terms" />
-            <label htmlFor="terms" className="ml-2">
-              Accept out <a href="">Terms</a> and <a href="">Conditions</a>
+            <label htmlFor="terms" className="ml-2 mb-4">
+              Accept our <a href="">Terms</a> and <a href="">Conditions</a>
             </label>
           </div>
           <input
             type="submit"
             value="Sign-Up"
-            className="btn btn-secondary mb-4 w-3/4"
+            className="btn btn-secondary mb-4 w-full"
           />
         </form>
         {signUpError && <p className="text-red-600">{signUpError}</p>}
